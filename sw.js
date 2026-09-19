e81d64cc9bf9d0abbfa641168b5be95bf6b01349
reccoded <recmeins@gmail.com>
2026-09-19 19:51:25 +0900

    Delete sw.js

--- sw.js
+++
@@ -1,1 +0,0 @@
-const C='revision-app-v1';self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(['./index.html','./manifest.webmanifest']))));self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));self.addEventListener('fetch',e=>{if(e.request.method==='GET')e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))})
